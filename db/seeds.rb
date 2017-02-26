3cdae8efa224fb6c6e97da179c37e79d1fb36eb4
Yen <yenchieh86@hotmail.com>
2017-02-26 20:46:17 +0000

    Add seed into seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,173 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+password = '111111'
+
+user = User.new(
+    email: "admin@gmail.com",
+    password: password,
+    password_confirmation: password,
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    username: Faker::Internet.user_name,
+    birthday: Faker::Date.backward(14),
+    role: 'admin'
+  )
+  
+  user.skip_confirmation!
+  user.save
+
+5.times do
+  user = User.new(
+    email: Faker::Internet.free_email,
+    password: password,
+    password_confirmation: password,
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    username: Faker::Internet.user_name,
+    birthday: Faker::Date.backward(14),
+    role: 'standard'
+  )
+  
+  user.skip_confirmation!
+  user.save
+
+  seller = User.new(
+    email: Faker::Internet.free_email,
+    password: password,
+    password_confirmation: password,
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    username: Faker::Internet.user_name,
+    birthday: Faker::Date.backward(14),
+    role: 'seller'
+  )
+  
+  seller.skip_confirmation!
+  seller.save
+end
+
+
+
+Category.create!(title: 'Food', body: 'This Category is for all kinds of food.')
+
+category = Category.last
+
+2.times do 
+  Item.create!(
+    title: Faker::Food.ingredient,
+    body: "This attribute is for food's description.",
+    user_id: Faker::Number.between(7, 10),
+    quantity: Faker::Number.number(2),
+    category_id: category.id,
+    price: Faker::Number.decimal(3, 2),
+    status: 'off_shelf'
+  )
+  
+  WishList.create!(user_id: Faker::Number.between(7, 10), item_id: Item.last.id)
+  
+  Review.create!(
+    rating: Faker::Number.between(1, 5),
+    body: Faker::Hipster.sentence,
+    user_id: Faker::Number.between(7, 10),
+    item_id: Item.last.id
+  )
+  
+  Item.create!(
+    title: Faker::Food.ingredient,
+    body: "This attribute is for food's description.",
+    user_id: Faker::Number.between(7, 10),
+    quantity: Faker::Number.number(2),
+    category_id: category.id,
+    price: Faker::Number.decimal(3, 2),
+    status: 'on_shelf'
+  )
+  
+  WishList.create!(user_id: Faker::Number.between(7, 10), item_id: Item.last.id)
+  
+  Review.create!(
+    rating: Faker::Number.between(1, 5),
+    body: Faker::Hipster.sentence,
+    user_id: Faker::Number.between(7, 10),
+    item_id: Item.last.id
+  )
+  
+  Item.create!(
+    title: Faker::Food.ingredient,
+    body: "This attribute is for food's description.",
+    user_id: Faker::Number.between(7, 10),
+    quantity: Faker::Number.number(2),
+    category_id: category.id,
+    price: Faker::Number.decimal(3, 2),
+    status: 'special_offer'
+  )
+  
+  WishList.create!(user_id: Faker::Number.between(7, 10), item_id: Item.last.id)
+  
+  Review.create!(
+    rating: Faker::Number.between(1, 5),
+    body: Faker::Hipster.sentence,
+    user_id: Faker::Number.between(7, 10),
+    item_id: Item.last.id
+  )
+end
+
+Category.create!(title: 'Book', body: 'This Category is for all kinds of books.')
+
+category = Category.last
+
+2.times do 
+  Item.create!(
+    title: Faker::Book.title,
+    body: "This attribute is for books's description.",
+    user_id: Faker::Number.between(7, 10),
+    quantity: Faker::Number.number(2),
+    category_id: category.id,
+    price: Faker::Number.decimal(3, 2),
+    status: 'off_shelf'
+  )
+  
+  WishList.create!(user_id: Faker::Number.between(7, 10), item_id: Item.last.id)
+  
+  Review.create!(
+    rating: Faker::Number.between(1, 5),
+    body: Faker::Hipster.sentence,
+    user_id: Faker::Number.between(7, 10),
+    item_id: Item.last.id
+  )
+  
+  Item.create!(
+    title: Faker::Book.title,
+    body: "This attribute is for book's description.",
+    user_id: Faker::Number.between(7, 10),
+    quantity: Faker::Number.number(2),
+    category_id: category.id,
+    price: Faker::Number.decimal(3, 2),
+    status: 'on_shelf'
+  )
+  
+  WishList.create!(user_id: Faker::Number.between(7, 10), item_id: Item.last.id)
+  
+  Review.create!(
+    rating: Faker::Number.between(1, 5),
+    body: Faker::Hipster.sentence,
+    user_id: Faker::Number.between(7, 10),
+    item_id: Item.last.id
+  )
+  
+  Item.create!(
+    title: Faker::Book.title,
+    body: "This attribute is for book's description.",
+    user_id: Faker::Number.between(7, 10),
+    quantity: Faker::Number.number(2),
+    category_id: category.id,
+    price: Faker::Number.decimal(3, 2),
+    status: 'special_offer'
+  )
+  
+  WishList.create!(user_id: Faker::Number.between(7, 10), item_id: Item.last.id)
+  
+  Review.create!(
+    rating: Faker::Number.between(1, 5),
+    body: Faker::Hipster.sentence,
+    user_id: Faker::Number.between(7, 10),
+    item_id: Item.last.id
+  )
+end
