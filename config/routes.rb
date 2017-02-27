8f6fa8aa5b7ce16a3e9ee08e4541f89faff83999
Yen <yenchieh86@hotmail.com>
2017-02-27 02:33:14 +0000

    fix order model

--- config/routes.rb
+++ config/routes.rb
@@ -21,7 +21,6 @@ Rails.application.routes.draw do
   end
   
   resources :items, only: [:show, :new, :create, :edit, :update, :destroy]
-  
   resources :reviews, only: [:destroy]
   
   resources :items, only: [] do
