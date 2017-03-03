6e8b2c110085bd8bb697dfe48a34353c5df75c0c
Yen <yenchieh86@hotmail.com>
2017-03-03 05:18:50 +0000

    add back rspec

--- spec/controllers/static_pages_controller_spec.rb
+++ spec/controllers/static_pages_controller_spec.rb
@@ -2,6 +2,28 @@ require 'rails_helper'
 
 RSpec.describe StaticPagesController, type: :controller do
 
- 
+  describe "GET #contect" do
+    it "renders to contect page" do
+      get :contect
+      expect(response).to be_success
+      expect(response).to have_http_status(200)
+    end
+  end
+  
+  describe "GET #help" do
+    it "renders to help page" do
+      get :help
+      expect(response).to be_success
+      expect(response).to have_http_status(200)
+    end
+  end
+  
+  describe "GET #home" do
+    it "renders to home page" do
+      get :home
+      expect(response).to be_success
+      expect(response).to have_http_status(200)
+    end
+  end
 
 end
